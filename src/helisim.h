5434b38d0606fab1823c745d28991880fc88ab91
Dan Malta <malta.dan@gmail.com>
2015-03-09 20:42:00 +0200

    Create helisim.h

---
+++ src/helisim.h
@@ -0,0 +1,27 @@
+#ifndef helisim_h
+#define helisim_h
+
+#include "glos.h" // MS specific stuff
+
+#include <stdio.h>
+#include <GL/gl.h> // system OpenGL includes
+#include <GL/glu.h>
+//#include <GL/glaux.h>
+#include <math.h>
+#include <GL/glut.h>
+#include <mmsystem.h>
+
+
+
+#include "matlib.h"
+#include "model.h"
+#include "graphics.h"
+#include "instruments.h"
+
+
+/******** GLOBAL DECLARATIONS **********/
+extern double windows_dt;
+
+
+
+#endif
